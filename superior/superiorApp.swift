223520a5f89fd6314bb687b4f5d58ea422d4e223
ItsMyDay797 <m.rus1298@gmail.com>
2022-07-21 00:05:16 +0300

    Initial Commit

---
+++ superior/superiorApp.swift
@@ -0,0 +1,17 @@
+//
+//  superiorApp.swift
+//  superior
+//
+//  Created by Марк Русаков on 21.07.22.
+//
+
+import SwiftUI
+
+@main
+struct superiorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
