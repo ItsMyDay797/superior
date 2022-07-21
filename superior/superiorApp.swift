3bc7a07a653536c509573d9ded8b3b9e8e292148
ItsMyDay797 <m.rus1298@gmail.com>
2022-07-21 18:04:41 +0300

    Added firebase

--- superior/superiorApp.swift
+++ superior/superiorApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct superiorApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             LandingView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("setting up firebase")
+        return true
     }
 }
